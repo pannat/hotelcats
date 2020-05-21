f6ed0860b84ed2a2e084b875dba178d9136bfb47
Natalie <pannat.job@gmail.com>
2020-05-21 21:48:48 +0300

    set adaptive photo

--- src/components/rooms-push/rooms-push.js
+++ src/components/rooms-push/rooms-push.js
@@ -2,17 +2,15 @@ import Swipe from 'swipejs';
 
 const mySwipe = new Swipe(document.querySelector('.rooms-push__slider'), {
     startSlide: 0,
-    speed: 400,
-    // auto: 3000,
+    speed: 200,
     draggable: false,
     continuous: true,
     disableScroll: false,
     stopPropagation: false,
     ignore: ".scroller",
-    callback: function(index, elem, dir) {
+    callback: function(index) {
         document.getElementById(`${index}` + "room").checked = true;
-    },
-    transitionEnd: function(index, elem) {}
+    }
 });
 
 const controls = document.querySelector('.rooms-push .slider__controls');
