0b42abc6b60ef88f29804ba4a37e0282bbfa33f6
Natalie <pannat.job@gmail.com>
2020-06-08 11:15:03 +0300

    slider rooms full done

--- src/components/rooms-push/rooms-push.js
+++ src/components/rooms-push/rooms-push.js
@@ -1,19 +1,32 @@
 import Swipe from 'swipejs';
 
+const controls = document.querySelector('.slider__dots');
+const nextButton = document.querySelector('.slider__button--next');
+const prevButton = document.querySelector('.slider__button--prev');
+
 const mySwipe = new Swipe(document.querySelector('.rooms-push__slider'), {
     startSlide: 0,
     speed: 200,
     draggable: false,
     continuous: true,
     disableScroll: false,
-    stopPropagation: false,
+    stopPropagation: true,
     ignore: ".scroller",
     callback: function(index) {
         document.getElementById(`${index}room`).checked = true;
     }
 });
 
-const controls = document.querySelector('.rooms-push .slider__controls');
 controls.addEventListener('change', (evt) => {
-    mySwipe.slide(evt.target.id[0])
+    mySwipe.slide(evt.target.id[0]);
+})
+
+nextButton.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    mySwipe.next();
+})
+
+prevButton.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    mySwipe.prev();
 })
